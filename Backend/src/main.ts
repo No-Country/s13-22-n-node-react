8d1f8023cd1261e5ec90a86041932b9e88af812c
Ferwin Arias <ferwin.arias@gmail.com>
2024-02-08 13:34:57 -0500

    Setting the initial Nest server, setting the Docker containerization and adding the swagger documentation setting

---
+++ Backend/src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configservice = app.get<ConfigService>(ConfigService);
+  const port = configservice.get('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('Food App API')
+    .setDescription('This is an API for the Food APP')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(port);
+}
+bootstrap();
