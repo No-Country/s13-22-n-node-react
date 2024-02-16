468504e9ac00fab0f8710e01c788ccdc2ae56f2f
SofiDevO <angiemeow89@gmail.com>
2024-02-16 04:06:12 -0600

    Se crea compoente header/se agregan variables y reset.css

--- Frontend/s13-22-client-side/src/components/Header/Header.jsx
+++ Frontend/s13-22-client-side/src/components/Header/Header.jsx
@@ -1,33 +1,86 @@
+import { useEffect } from "react";
+import "./Header.css";
+export const Header = () => {
+  useEffect(() => {
+    const handleClick = (e) => {
+      const panel = document.querySelector(".panel");
+      const panelBtn = document.querySelector(".panel-btn");
 
-export const Header = ()=>{
-    return(
-        <>
-        <header class="header">
-        <div class="header__container">
-            <button class="hamburger hamburger--spring
-            " type="button">
-                <span class="hamburger-box">
-                  <span class="hamburger-inner"></span>
-                </span>
-              </button>
-            <div class="logo__nav">
-                <h3 class="header__logo">HungryTime</h3>
-                <nav class="header__menu ">
-                    <ul class="menu__list panel">
-                        <li class="menu__item"><a href="#" class="menu__link">Hamburguesas</a></li>
-                        <li class="menu__item"><a href="#" class="menu__link">Pizza</a></li>
-                        <li class="menu__item"><a href="#" class="menu__link">Pollo</a></li>
-                        <li class="menu__item"><a href="#about" class="menu__link">Acerca</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="header__icons">
-                <a href="#"><img class="icon__img--login" src="../../../public/svg/Login.svg" alt="Login"/></a>
-                <a href="#"><img class="icon__img--cart" src="../../../public/svg/cart.svg" alt="Cart"/></a>
-            </div>
+      if (e.target.matches(".panel-btn") || e.target.matches(".panel-btn *")) {
+        panel.classList.toggle("is-active");
+        panelBtn.classList.toggle("is-active");
+      }
+
+      if (e.target.matches(".menu__link")) {
+        panel.classList.remove("is-active");
+        panelBtn.classList.remove("is-active");
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+  return (
+    <>
+      <header className="header">
+        <div className="header__container">
+          <button
+            className="hamburger hamburger--spring panel-btn
+            "
+            type="button"
+          >
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
+          <div className="logo__nav">
+            <h3 className="header__logo">HungryTime</h3>
+            <nav className="header__menu panel">
+              <ul className="menu__list ">
+                <li className="menu__item">
+                  <a href="#" className="menu__link">
+                    Hamburguesas
+                  </a>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">
+                    Pizza
+                  </a>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">
+                    Pollo
+                  </a>
+                </li>
+                <li className="menu__item">
+                  <a href="#about" className="menu__link">
+                    Acerca
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="header__icons">
+            <a href="#">
+              <img
+                className="icon__img--login"
+                src="../../../public/svg/Login.svg"
+                alt="Login"
+              />
+            </a>
+            <a href="#">
+              <img
+                className="icon__img--cart"
+                src="../../../public/svg/cart.svg"
+                alt="Cart"
+              />
+            </a>
+          </div>
         </div>
-    </header>
-
-        </>
-    )
-}
+      </header>
+    </>
+  );
+};
