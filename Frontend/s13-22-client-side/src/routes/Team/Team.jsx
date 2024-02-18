38d2895747ee84ef610ea9898f41b9e9a284c0bc
SofiDevO <angiemeow89@gmail.com>
2024-02-18 02:14:30 -0600

    Se crea panel dropdown para loginIcon

--- Frontend/s13-22-client-side/src/routes/Team/Team.jsx
+++ Frontend/s13-22-client-side/src/routes/Team/Team.jsx
@@ -1,9 +1,15 @@
 import { Header } from "../../components/Header/Header"
 
 export const Team = ()=>{
+    const links = [
+        { text: "Hamburguesas", url: "#" },
+        { text: "Pizza", url: "#" },
+        { text: "Pollo", url: "#" },
+        { text: "Acerca", url: "/#about" }
+      ];
     return(
         <>
-        <Header/>
+        <Header  links={links}/>
         <main>
         <h1>Nuestro equipo</h1>
         </main>
