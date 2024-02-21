f7ba9d77fd4e20f0e96b831fe9d2c423d82224ec
SofiDevO <angiemeow89@gmail.com>
2024-02-20 18:28:26 -0600

    Se agrega data complementaria

--- Frontend/s13-22-client-side/src/components/MenuMain/Menu.jsx
+++ Frontend/s13-22-client-side/src/components/MenuMain/Menu.jsx
@@ -1,4 +1,4 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import '../MenuMain/Menu.css';
 import menuData from '../../data/menuData';
 import FoodCard from '../FoodCard/FoodCard';
@@ -14,22 +14,21 @@ function Menu() {
         <section className='menu-section'>
             {/* Mapeo de cada categoría */}
             {uniqueCategories.map(category => (
-                <>
-                    <div key={category} className='type-name-container'>
+                <div key={category} className='category-container'>
+                    <div className='type-name-container'>
                         <h4 className='food-name' id={category}>{category}</h4>
                         <Link to="/" className="more__container">
-                        <p className='seeMore'>Ver más</p>
-                        <img src="../../../public/svg/arrowIcon.svg" alt="" />
+                            <p className='seeMore'>Ver más</p>
+                            <img src="../../../public/svg/arrowIcon.svg" alt="" />
                         </Link>
-
                     </div>
                     <div className='cards-container'>
                         {/* Filtrar elementos del menú por categoría */}
                         {menu.filter(item => item.category === category).splice(0, 4).map(filteredItem => (
                             <FoodCard key={filteredItem.id} item={filteredItem} />
-                            ))}
+                        ))}
                     </div>
-            </>
+                </div>
             ))}
         </section>
     );
