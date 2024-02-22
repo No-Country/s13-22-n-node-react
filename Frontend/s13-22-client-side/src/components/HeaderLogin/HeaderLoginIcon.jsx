99aab17bb21527dbd29ea1d539b609c4e6c7f66e
DiegoRivasDev <diegodevsoftware@gmail.com>
2024-02-22 18:59:51 -0400

    formularios de login, register, addproducts

--- Frontend/s13-22-client-side/src/components/HeaderLogin/HeaderLoginIcon.jsx
+++ Frontend/s13-22-client-side/src/components/HeaderLogin/HeaderLoginIcon.jsx
@@ -1,61 +1,69 @@
-import * as React from 'react';
+import * as React from "react";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import Tooltip from "@mui/material/Tooltip";
-import "./HeaderLoginIcon.css"
-import { Link } from 'react-router-dom';
+import "./HeaderLoginIcon.css";
+import { Link } from "react-router-dom";
 const settings = [
-    { desc: "Profile", link: "/team" },
-    { desc: "Account", link: "/" },
-    { desc: "Dashboard", link: "#" },
-    { desc: "Logout", link: "/#about" }
-  ];
-
+  { desc: "Profile", link: "/team" },
+  { desc: "Account", link: "/" },
+  { desc: "Dashboard", link: "#" },
+  { desc: "Logout", link: "/#about" },
+];
 
 export const HeaderLogin = () => {
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-      };
-
-      const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-      };
-
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
     <>
-    <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Opciones">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <img className='icon__img--login' alt="Login Icon" src="../../../public/svg/Login.svg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >{settings.map((setting, index) => (
-                <li className='user__list' key={`${setting.desc}-${index}`} onClick={handleCloseUserMenu}>
-                  <Link className='user__link' to={setting.link}>{setting.desc}</Link>
-                </li>
-              ))}
-            </Menu>
-          </Box>
+      <Box sx={{ flexGrow: 0 }}>
+        <Tooltip title="Opciones">
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <img
+              className="icon__img--login"
+              alt="Login Icon"
+              src="../../../public/svg/Login.svg"
+            />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          sx={{ mt: "45px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {settings.map((setting, index) => (
+            <li
+              className="user__list"
+              key={`${setting.desc}-${index}`}
+              onClick={handleCloseUserMenu}
+            >
+              <Link className="user__link" to={setting.link}>
+                {setting.desc}
+              </Link>
+            </li>
+          ))}
+        </Menu>
+      </Box>
     </>
   );
 };
--- Frontend/s13-22-client-side/src/components/LogOut/LogOut.jsx
+++ Frontend/s13-22-client-side/src/components/LogOut/LogOut.jsx
@@ -1,60 +1,68 @@
-import * as React from 'react';
+import * as React from "react";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import Tooltip from "@mui/material/Tooltip";
-import "../HeaderLogin/HeaderLoginIcon.css"
-import "./LogOut.css"
-import { Link } from 'react-router-dom';
+import "../HeaderLogin/HeaderLoginIcon.css";
+import "./LogOut.css";
+import { Link } from "react-router-dom";
 const settings = [
-    { desc: "Iniciar Sesión", link: "/team" },
-    { desc: "crear cuenta", link: "/" },
-  ];
+  { desc: "Iniciar Sesión", link: "/login" },
+  { desc: "crear cuenta", link: "/registro" },
+];
 
+export const LogOut = () => {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-export const LogOut  = () => {
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-      };
-
-      const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-      };
-
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
     <>
-    <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Opciones">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <img className='icon__img--login' alt="Login Icon" src="../../../public/svg/Login.svg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >{settings.map((setting, index) => (
-                <li className='user__list' key={`${setting.desc}-${index}`} onClick={handleCloseUserMenu}>
-                  <Link className='user__link' to={setting.link}>{setting.desc}</Link>
-                </li>
-              ))}
-            </Menu>
-          </Box>
+      <Box sx={{ flexGrow: 0 }}>
+        <Tooltip title="Opciones">
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <img
+              className="icon__img--login"
+              alt="Login Icon"
+              src="../../../public/svg/Login.svg"
+            />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          sx={{ mt: "45px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {settings.map((setting, index) => (
+            <li
+              className="user__list"
+              key={`${setting.desc}-${index}`}
+              onClick={handleCloseUserMenu}
+            >
+              <Link className="user__link" to={setting.link}>
+                {setting.desc}
+              </Link>
+            </li>
+          ))}
+        </Menu>
+      </Box>
     </>
   );
 };
