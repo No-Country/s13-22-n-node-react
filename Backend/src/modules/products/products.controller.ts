21a39d4168956aeb38cfc6147e856ec63c09c84b
Edwin Fernando Arias Arrieta <86627424+ferwinred@users.noreply.github.com>
2024-03-11 14:23:01 -0500

    Update products.controller.ts

--- Backend/src/modules/products/products.controller.ts
+++ Backend/src/modules/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseFilters } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseFilters, ParseUUIDPipe } from '@nestjs/common';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
