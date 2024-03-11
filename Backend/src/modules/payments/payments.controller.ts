d2623805ab499edc24e3b9271fca8020b57fbb5f
Edwin Fernando Arias Arrieta <86627424+ferwinred@users.noreply.github.com>
2024-03-11 14:22:50 -0500

    Update payments.controller.ts

--- Backend/src/modules/payments/payments.controller.ts
+++ Backend/src/modules/payments/payments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseFilters } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseFilters, ParseUUIDPipe } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
